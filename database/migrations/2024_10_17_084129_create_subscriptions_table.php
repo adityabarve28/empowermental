ca7f95ec5cda3dd43f4b302c5fe6f2b0faf158d0
adityabarve28 <adityabarve28@gmail.com>
2025-01-22 15:45:28 +0530

    Updated Subscriptions migration

--- database/migrations/2024_10_17_084129_create_subscriptions_table.php
+++ database/migrations/2024_10_17_084129_create_subscriptions_table.php
@@ -23,11 +23,6 @@ class CreateSubscriptionsTable extends Migration
             $table->integer('status')->nullable(); // int DEFAULT NULL
             $table->timestamps(); // created_at, updated_at as timestamps
             $table->unsignedBigInteger('therapist_id')->nullable(); // bigint unsigned DEFAULT NULL
-
-            // Foreign key constraints
-            $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade'); // FOREIGN KEY (institute_id) REFERENCES institutes(id) ON DELETE CASCADE
-            $table->foreign('plan_id')->references('id')->on('subscription_plans')->onDelete('cascade')->onUpdate('cascade'); // FOREIGN KEY (plan_id) REFERENCES subscription_plans(id) ON DELETE CASCADE ON UPDATE CASCADE
-            $table->foreign('therapist_id')->references('id')->on('counselors')->nullable(); // FOREIGN KEY (therapist_id) REFERENCES counselors(id) with NULLABLE
         });
     }
 
