68ed6d2a7b2b0e83c229425c0504ec96ed86e94c
adityabarve28 <adityabarve28@gmail.com>
2025-01-29 23:03:53 +0530

    updated entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to check database readiness
+# Wait for database readiness
 wait_for_database() {
     echo "Waiting for the database to be ready..."
     until php -r "new PDO('mysql:host=${DB_HOST:-database};port=${DB_PORT:-3306};dbname=${DB_DATABASE:-test}', '${DB_USERNAME:-root}', '${DB_PASSWORD:-}');" 2>/dev/null; do
@@ -10,37 +10,37 @@ wait_for_database() {
     echo "Database is ready!"
 }
 
-# Wait for the database
-wait_for_database
+# Wait for Redis readiness
+wait_for_redis() {
+    echo "Waiting for Redis to be ready..."
+    until nc -z "${REDIS_HOST:-redis}" "${REDIS_PORT:-6379}"; do
+        sleep 1
+        echo "Still waiting for Redis..."
+    done
+    echo "Redis is ready!"
+}
 
-# Install Composer dependencies if vendor/autoload.php does not exist
+wait_for_database
+wait_for_redis
+
+# Install Composer dependencies
 if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction
+    composer install --no-progress --no-interaction --optimize-autoloader --no-dev
 fi
 
-# If .env does not exist, create it from .env.example
+# Create .env file if not exists
 if [ ! -f ".env" ]; then
     echo "Creating .env file for environment $APP_ENV"
     cp .env.example .env
-else
-    echo ".env file already exists"
 fi
 
-# Run Laravel migrations and other Artisan commands
-if php artisan migrate:fresh --seed; then
-    echo "Migrations ran successfully."
-else
-    echo "Migrations failed. Exiting."
-    exit 1
-fi
-
+# Run Artisan commands
 php artisan key:generate
+php artisan migrate --force
+php artisan optimize
 php artisan cache:clear
 php artisan config:clear
-php artisan route:clear
+php artisan route:cache
 
-# Serve the application on the specified port or fallback to 8080
-php artisan serve --port=${PORT:-8080} --host=0.0.0.0 --env=.env
-
-# Execute the default PHP entrypoint
-exec docker-php-entrypoint "$@"
+# Start Apache
+exec apache2-foreground
