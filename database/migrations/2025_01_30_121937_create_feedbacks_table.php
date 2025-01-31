e5dfaab94d8bba7087dd0de1e8ff1a33bc2241b1
adityabarve28 <adityabarve28@gmail.com>
2025-01-31 16:39:15 +0530

    added feedback migration

--- database/migrations/2025_01_30_121937_create_feedbacks_table.php
+++ database/migrations/2025_01_30_121937_create_feedbacks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('feedbacks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('to_id')->constrained('institutes');  // Assuming 'institutes' table exists
+            $table->int('user_id'); 
+            $table->int('to_id');  // Assuming 'institutes' table exists
             $table->string('to_name');
             $table->text('feedback');
             $table->timestamps();
